95b8d470e811551b0e59ba29e9ae749811e65e7c
amadarain <t.f.octo@gmail.com>
2016-11-20 05:21:07 +0900

    Make type of view selectable in ImageFiler

--- src/main/kotlin/com/github/am4dr/image/tagger/app/ImageFiler.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/app/ImageFiler.kt
@@ -6,11 +6,14 @@ import javafx.beans.property.SimpleListProperty
 import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
+import javafx.event.EventHandler
 import javafx.scene.Node
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.ListView
 import javafx.scene.control.ScrollPane
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.HBox
 import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import org.slf4j.Logger
@@ -49,5 +52,10 @@ class ImageFiler(val mainFrame: MainFrame) {
                         .then<Node>(mainFrame.makeDirectorySelectorPane())
                         .otherwise(selectedView))
     }
-    val node: Node = VBox(Label("画像ファイラー 仮置きテキスト"), currentView)
+    val node: Node = VBox(
+            Label("画像ファイラー 仮置きテキスト"),
+            HBox(
+                Button("リスト").apply { onAction = EventHandler { selectedView.set(listNode) } },
+                Button("サムネイル").apply { onAction = EventHandler { selectedView.set(thumbnailNode) } }),
+            currentView)
 }
