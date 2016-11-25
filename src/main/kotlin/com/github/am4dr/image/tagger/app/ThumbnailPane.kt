1ad176dcb4215bfa6582cf412b6af269c529b4f8
amadarain <t.f.octo@gmail.com>
2016-11-25 09:00:24 +0900

    Replace expanding method with overlay
    for reducing code complexity

--- src/main/kotlin/com/github/am4dr/image/tagger/app/ThumbnailPane.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/app/ThumbnailPane.kt
@@ -2,13 +2,14 @@ package com.github.am4dr.image.tagger.app
 
 import javafx.beans.binding.Bindings
 import javafx.beans.binding.When
-import javafx.beans.property.*
-import javafx.beans.value.ObservableObjectValue
+import javafx.beans.property.ListProperty
+import javafx.beans.property.Property
+import javafx.beans.property.SimpleListProperty
+import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.FXCollections
 import javafx.event.EventHandler
 import javafx.geometry.Pos
 import javafx.geometry.VPos
-import javafx.scene.Node
 import javafx.scene.control.ScrollPane
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
@@ -19,13 +20,14 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Callable
 
-class ThumbnailPane(children: ListProperty<ImageData>) {
+class ThumbnailPane(imageDataList: ListProperty<ImageData>) {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
     private val imagesProperty = SimpleListProperty<ImageData>()
-    var children: ListProperty<ImageData>
+    var imageData: ListProperty<ImageData>
         get() = imagesProperty
         set(value) = imagesProperty.bind(value)
-    val pane = ScrollPane().apply {
+    val pane: Pane
+    private val sp = ScrollPane().apply {
         fitToWidthProperty().set(true)
         hbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
     }
@@ -33,18 +35,16 @@ class ThumbnailPane(children: ListProperty<ImageData>) {
     private val selectedTileProperty = SimpleObjectProperty<ImageTile>().apply {
         addListener { obs, old, new -> log.debug("change selectedTileProperty: $old -> $new") }
     }
-    private val nextRowStartIndexProperty = SimpleIntegerProperty().apply {
-        addListener { observableValue, old, new -> log.debug("next row index of selected row: $new") }
-    }
-    private val expandedTile: ExpandedTile
+    private val overlayTile = OverlayTile()
     private val flowPane = FlowPane(10.0, 10.0).apply {
         alignment = Pos.CENTER
         rowValignment = VPos.BASELINE
     }
     init {
-        this.children = children
-        pane.content = flowPane
-        expandedTile = ExpandedTile(pane, selectedTileProperty)
+        this.imageData = imageDataList
+        pane = StackPane(
+                sp.apply { content = flowPane },
+                overlayTile.apply { visibleProperty().set(false) })
         tiles.bind(Bindings.createObjectBinding(
                 Callable { FXCollections.observableList(imagesProperty.filterNotNull().map(::ImageTile)) },
                 imagesProperty))
@@ -55,70 +55,49 @@ class ThumbnailPane(children: ListProperty<ImageData>) {
                 val tile = e.source
                 if (tile !is ImageTile) { return@EventHandler }
                 log.debug("tile clicked")
-                removeExpandedTile()
                 if (selectedTileProperty.get() === tile) {
                     selectedTileProperty.set(null)
                     return@EventHandler
                 }
                 selectedTileProperty.set(tile)
-                insertExpandedTile()
+                showOverlayTile()
             }
             new.map { tile -> tile.onMouseClicked = tileClickHandler }
-            val contents = flowPane.children
-            contents.setAll(new)
-            nextRowStartIndexProperty.apply {
-                val calc = Callable {
-                    val tile = selectedTileProperty.get()
-                    if (tile != null) calcNextRowStartIndex(contents, contents.indexOf(tile)) else 0
-                }
-                unbind()
-                bind(Bindings.createIntegerBinding(calc, selectedTileProperty, contents.last().layoutXProperty()))
-                addListener { observableValue, old, new ->
-                    insertExpandedTile()
-                }
-            }
+            flowPane.children.setAll(new)
+        }
+        overlayTile.apply {
+            image.bind(Bindings.createObjectBinding(
+                    Callable { selectedTileProperty.get()?.data?.tempImage },
+                    selectedTileProperty))
+            onMouseClicked = EventHandler<MouseEvent> { e -> hideOverlayTile() }
         }
     }
-    private fun removeExpandedTile() {
-        log.debug("remove ExpandTile")
-        selectedTileProperty.get() ?: return
-        flowPane.children.removeAll(expandedTile)
+    private fun showOverlayTile() {
+        overlayTile.visibleProperty().set(true)
     }
-    private fun insertExpandedTile(index: Int = -1) {
-        log.debug("insert ExpandTile at $index")
-        selectedTileProperty.get() ?: return
-        flowPane.children.removeAll(expandedTile)
-        if (index < 0) { flowPane.children.add(nextRowStartIndexProperty.get(), expandedTile) }
-        else if (index != 0) { flowPane.children.add(index, expandedTile) }
-        else { flowPane.children.add(expandedTile) }
+    private fun hideOverlayTile() {
+        overlayTile.visibleProperty().set(false)
+        selectedTileProperty.set(null)
     }
-    private fun calcNextRowStartIndex(nodes: List<Node>, index: Int): Int {
-        val selectedBaseline = nodes[index].boundsInParent.maxY
-        return (index+1..nodes.size-1)
-                .firstOrNull { nodes[it].boundsInParent.maxY > selectedBaseline + 10 } ?: 0
-    }
-}
-private class ExpandedTile(parent: Region, targetTile: ObservableObjectValue<ImageTile>) : VBox() {
-    private val imageView = ImageView().apply {
-        isPreserveRatio = true
-        fitWidthProperty().bind(
-                When(parent.widthProperty().multiply(2.0).lessThan(400.0))
-                        .then(0.9).otherwise(0.75)
-                        .multiply(parent.widthProperty()))
-        fitHeightProperty().bind(
-                When(parent.heightProperty().multiply(2.0).lessThan(400.0))
-                        .then(0.9).otherwise(0.75)
-                        .multiply(parent.heightProperty()))
-    }
-    val image: Property<Image>
-        get() = imageView.imageProperty()
-    init {
-        fillWidthProperty().set(true)
-        prefWidthProperty().bind(parent.widthProperty().subtract(40))
-        maxWidthProperty().bind(parent.widthProperty().subtract(40))
-        alignment = Pos.CENTER
-        background = Background(BackgroundFill(Color.rgb(30, 30, 30), null, null))
-        image.bind(Bindings.createObjectBinding(Callable { targetTile.get()?.data?.image }, targetTile))
-        children.add(imageView)
+    private class OverlayTile() : VBox() {
+        val imageView = ImageView().apply {
+            isPreserveRatio = true
+            fitWidthProperty().bind(
+                    When(this@OverlayTile.widthProperty().multiply(2.0).lessThan(400.0))
+                            .then(0.9).otherwise(0.75)
+                            .multiply(this@OverlayTile.widthProperty()))
+            fitHeightProperty().bind(
+                    When(this@OverlayTile.heightProperty().multiply(2.0).lessThan(400.0))
+                            .then(0.9).otherwise(0.75)
+                            .multiply(this@OverlayTile.heightProperty()))
+        }
+        val image: Property<Image>
+            get() = imageView.imageProperty()
+        init {
+            fillWidthProperty().set(true)
+            alignment = Pos.CENTER
+            background = Background(BackgroundFill(Color.rgb(30, 30, 30, 0.75), null, null))
+            children.add(imageView)
+        }
     }
 }
