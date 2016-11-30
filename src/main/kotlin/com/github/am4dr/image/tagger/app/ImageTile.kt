051d3fe18ec9c3604d3a2280baa1f1533f5845f2
amadarain <t.f.octo@gmail.com>
2016-11-30 15:21:49 +0900

    Change tag design of ImageTile for readability

--- src/main/kotlin/com/github/am4dr/image/tagger/app/ImageTile.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/app/ImageTile.kt
@@ -1,20 +1,37 @@
 package com.github.am4dr.image.tagger.app
 
-import javafx.geometry.Pos
+import javafx.geometry.Insets
+import javafx.scene.Node
+import javafx.scene.control.Label
 import javafx.scene.image.ImageView
-import javafx.scene.layout.Background
-import javafx.scene.layout.BackgroundFill
-import javafx.scene.layout.StackPane
-import javafx.scene.layout.VBox
+import javafx.scene.layout.*
 import javafx.scene.paint.Color
+import javafx.scene.text.Font
 
 class ImageTile(val data: ImageData) : StackPane() {
     init {
-        val overlay = VBox().apply {
-            alignment = Pos.CENTER
-            background = Background(BackgroundFill(Color.rgb(30, 30, 30, 0.75), null, null))
+        val image = ImageView(data.thumnail)
+        val overlay = AnchorPane().apply {
+            val tagsPane = FlowPane(7.5, 5.0)
+            tagsPane.children.addAll(data.tags.map(::createTagLabel))
+            val d = 10.0
+            AnchorPane.setTopAnchor(tagsPane, d)
+            AnchorPane.setLeftAnchor(tagsPane, d)
+            AnchorPane.setRightAnchor(tagsPane, d)
+            AnchorPane.setBottomAnchor(tagsPane, d)
+            children.add(tagsPane)
+            background = Background(BackgroundFill(Color.rgb(0, 0, 0, 0.5), null, null))
             visibleProperty().bind(this@ImageTile.hoverProperty())
+            prefWidthProperty().bind(image.image.widthProperty())
+            prefHeightProperty().bind(image.image.heightProperty())
         }
-        children.setAll(ImageView(data.thumnail), overlay)
+        children.setAll(image, overlay)
     }
 }
+private fun createTagLabel(name: String): Node =
+        Label(name).apply {
+            textFill = Color.rgb(200, 200, 200)
+            padding = Insets(-1.0, 2.0, 0.0, 2.0)
+            font = Font(14.0)
+            background = Background(BackgroundFill(Color.BLACK, CornerRadii(2.0), null))
+        }
