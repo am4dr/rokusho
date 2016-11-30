64b39973c981d38ab30e74d3034a8d27e0216910
amadarain <t.f.octo@gmail.com>
2016-11-30 14:11:49 +0900

    Display tags on ImageTile

--- src/main/kotlin/com/github/am4dr/image/tagger/app/ImageTile.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/app/ImageTile.kt
@@ -1,10 +1,20 @@
 package com.github.am4dr.image.tagger.app
 
+import javafx.geometry.Pos
 import javafx.scene.image.ImageView
-import javafx.scene.layout.Pane
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
 
-class ImageTile(val data: ImageData) : Pane() {
+class ImageTile(val data: ImageData) : StackPane() {
     init {
-        children.setAll(ImageView(data.thumnail))
+        val overlay = VBox().apply {
+            alignment = Pos.CENTER
+            background = Background(BackgroundFill(Color.rgb(30, 30, 30, 0.75), null, null))
+            visibleProperty().bind(this@ImageTile.hoverProperty())
+        }
+        children.setAll(ImageView(data.thumnail), overlay)
     }
 }
