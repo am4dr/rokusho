d6b2a3dac697d4894c31941b3167545b61f6b958
amadarain <t.f.octo@gmail.com>
2016-12-10 04:38:03 +0900

    Remove ThumbnailPane.selectedTilesProperty

--- src/main/kotlin/com/github/am4dr/image/tagger/app/ThumbnailPane.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/app/ThumbnailPane.kt
@@ -4,7 +4,6 @@ import com.github.am4dr.image.tagger.node.ImageOverlay
 import com.github.am4dr.image.tagger.node.ImageTile
 import com.github.am4dr.image.tagger.node.ImageTileScrollPane
 import com.github.am4dr.image.tagger.util.createEmptyListProperty
-import javafx.beans.binding.Bindings
 import javafx.beans.property.*
 import javafx.collections.FXCollections
 import javafx.collections.ListChangeListener
@@ -17,36 +16,28 @@ import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.concurrent.Callable
 
 class ThumbnailPane(imageDataList: ListProperty<ImageData>) {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
     private val imagesProperty = createEmptyListProperty<ImageData>()
     val view = ThumbnailPaneView()
-    private val selectedTileProperty = SimpleObjectProperty<ImageTile>().apply {
-        addListener { obs, old, new -> log.debug("change selectedTileProperty: $old -> $new") }
-    }
     private val tiles = createEmptyListProperty<ImageTile>()
     init {
         imagesProperty.bind(imageDataList)
         val tileClickHandler = EventHandler<MouseEvent> { e ->
             val tile = e.source as? ImageTile ?: return@EventHandler
             log.info("tile clicked: $tile")
-            selectedTileProperty.set(tile)
+            view.overlayImageProperty.bind(SimpleObjectProperty(tile.data.tempImage))
             view.overlayVisibleProperty.set(true)
         }
         imagesProperty.addListener(ListChangeListener {
             val newTiles = imagesProperty.map {
                 ImageTile(it).apply { onMouseClicked = tileClickHandler }
             }
-            selectedTileProperty.set(null)
             tiles.setAll(FXCollections.observableList(newTiles))
             log.debug("tiles changed - new.size: ${tiles.size}")
         })
         view.tilesProperty.bind(tiles)
-        view.overlayImageProperty.bind(Bindings.createObjectBinding(
-                    Callable { selectedTileProperty.get()?.data?.tempImage },
-                    selectedTileProperty))
     }
 }
 class ThumbnailPaneView : StackPane() {
