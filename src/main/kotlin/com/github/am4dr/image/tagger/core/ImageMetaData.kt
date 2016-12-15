5000c15b729d793eeda07ea5044eb4a3be2dcbab
amadarain <t.f.octo@gmail.com>
2016-12-16 05:27:50 +0900

    Fix path of ImageMetaData was not normalized

--- src/main/kotlin/com/github/am4dr/image/tagger/core/ImageMetaData.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/core/ImageMetaData.kt
@@ -9,6 +9,6 @@ data class ImageMetaData(val tags: MutableList<out String> = mutableListOf())
 fun parseImageMetaData(tokens: String): Pair<Path, ImageMetaData> =
         parseImageMetaData(tokens.split("\t"))
 fun parseImageMetaData(tokens: List<String>): Pair<Path, ImageMetaData> =
-        Pair(Paths.get(tokens.first()), ImageMetaData(tokens.drop(1).toMutableList()))
+        Pair(Paths.get(tokens.first()).normalize(), ImageMetaData(tokens.drop(1).toMutableList()))
 fun loadImageMataData(file: File): MutableMap<Path, ImageMetaData> =
         file.readLines().associateTo(mutableMapOf<Path, ImageMetaData>(), ::parseImageMetaData)
