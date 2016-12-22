eaf96e2c16ce1a101d45b609c1c48a2f38d8d0ac
amadarain <t.f.octo@gmail.com>
2016-12-22 18:07:05 +0900

    Add MainModel

--- src/main/kotlin/com/github/am4dr/image/tagger/app/Main.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/app/Main.kt
@@ -1,6 +1,11 @@
 package com.github.am4dr.image.tagger.app
 
+import com.github.am4dr.image.tagger.core.Library
+import com.github.am4dr.image.tagger.core.Picture
 import javafx.application.Application
+import javafx.beans.binding.Bindings.createObjectBinding
+import javafx.beans.property.*
+import javafx.collections.FXCollections.observableList
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
@@ -8,6 +13,8 @@ import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Options
 import org.slf4j.LoggerFactory
+import java.nio.file.Path
+import java.util.concurrent.Callable
 
 fun main(args: Array<String>) = Application.launch(Main::class.java, *args)
 
@@ -20,6 +27,7 @@ fun main(args: Array<String>) = Application.launch(Main::class.java, *args)
 class Main : Application() {
     private val log = LoggerFactory.getLogger(this.javaClass)
     private val options = makeOptions()
+    private val MainModel = MainModel()
 
     override fun init() {
         log.info("launched with the params: ${parameters.raw}")
@@ -36,4 +44,21 @@ class Main : Application() {
     private fun makeOptions(): Options = with(Options()) {
         addOption(null, "saveto", true, "specify the directory path to save the tag file")
     }
+}
+
+// TODO 複数Libraryへの対応
+class MainModel {
+    private val _libraryProperty: ObjectProperty<Library>
+    val libraryProperty: ReadOnlyObjectProperty<Library>
+    val pictures: ReadOnlyListProperty<Picture>
+    init {
+        _libraryProperty = SimpleObjectProperty()
+        libraryProperty = SimpleObjectProperty<Library>()
+        libraryProperty.bind(_libraryProperty)
+        pictures = SimpleListProperty()
+        pictures.bind(createObjectBinding(
+                Callable { observableList(libraryProperty.get()?.pictures ?: mutableListOf()) },
+                libraryProperty))
+    }
+    fun setLibrary(path: Path) = _libraryProperty.set(Library(path))
 }
