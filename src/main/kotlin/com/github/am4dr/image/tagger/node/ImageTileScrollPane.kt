cc1ac7eb0e8635dcf11a8bc3264cfa76f27ffb65
amadarain <t.f.octo@gmail.com>
2017-01-07 00:34:59 +0900

    Simplify visibility control in ImageTileScrollPane
    The previous implementation of the visibility control (which used TileRange)
    was not suitable for the frequent change of pictureProperty.
    
    This simple implementation has enough speed.

--- src/main/kotlin/com/github/am4dr/image/tagger/node/ImageTileScrollPane.kt
+++ src/main/kotlin/com/github/am4dr/image/tagger/node/ImageTileScrollPane.kt
@@ -4,7 +4,9 @@ import com.github.am4dr.image.tagger.core.Picture
 import com.github.am4dr.image.tagger.util.TransformedList
 import com.github.am4dr.image.tagger.util.createEmptyListProperty
 import javafx.beans.binding.Bindings
-import javafx.beans.property.*
+import javafx.beans.property.ListProperty
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleListProperty
 import javafx.event.EventHandler
 import javafx.geometry.Insets
 import javafx.geometry.Pos
@@ -21,13 +23,17 @@ class ImageTileScrollPane(val tileFactory: (Picture) -> ImageTile = ::ImageTile)
     var onTileClicked: (ImageTile, Picture) -> Unit = { tile, pic -> }
 
     private val vValueHeightProperty = SimpleDoubleProperty()
-    private var ranges = mutableListOf<TileRange>()
-    private val maxRangeSize: Int = 200
     init {
         picturesProperty = createEmptyListProperty()
+        val margin = SimpleDoubleProperty(2000.0)
+        val screenTop = margin.multiply(-1).add(vValueHeightProperty)
+        val screenBottom = margin.add(vValueHeightProperty)
         tilesProperty = SimpleListProperty(TransformedList(picturesProperty) { pic ->
             tileFactory(pic).apply {
                 onMouseClicked = EventHandler<MouseEvent> { onTileClicked(this, pic) }
+                visibleProperty().bind(
+                        layoutYProperty().greaterThanOrEqualTo(screenTop)
+                                .and(layoutYProperty().lessThanOrEqualTo(screenBottom)))
             }
         })
         fitToWidthProperty().set(true)
@@ -42,34 +48,5 @@ class ImageTileScrollPane(val tileFactory: (Picture) -> ImageTile = ::ImageTile)
                 it.consume()
             }
         }
-        val margin = SimpleDoubleProperty(2000.0)
-        val screenTop = margin.multiply(-1).add(vValueHeightProperty)
-        val screenBottom = margin.add(vValueHeightProperty)
-        tilesProperty.addListener { tiles, old, new ->
-            if (new != null && new.size < 1) { ranges.clear(); return@addListener }
-            val l = mutableListOf<TileRange>()
-            (0..new.size/maxRangeSize).forEach { n ->
-                val r = n*maxRangeSize..Math.min((n+1)*maxRangeSize-1, new.size-1)
-                val tr = TileRange(new.slice(r))
-                tr.imageVisibleProperty.bind(
-                        Bindings.and(
-                                screenTop.lessThanOrEqualTo(tr.lastTileProperty.value.layoutYProperty()),
-                                screenBottom.greaterThanOrEqualTo(tr.firstTileProperty.value.layoutYProperty())))
-                log.debug("TileRange created for: $r")
-                tr.imageVisibleProperty.addListener { value, old, new ->
-                    log.debug("TileRange $r : ${if(new) "visible" else "invisible"}")
-                }
-                l.add(tr)
-            }
-            ranges = l
-        }
-    }
-}
-private class TileRange(val list: List<ImageTile>) {
-    val imageVisibleProperty: BooleanProperty = SimpleBooleanProperty(true)
-    val firstTileProperty: Property<ImageTile> = SimpleObjectProperty<ImageTile>(list.first())
-    val lastTileProperty: Property<ImageTile> = SimpleObjectProperty<ImageTile>(list.last())
-    init {
-        this.list.forEach { it.imageVisibleProperty.bind(imageVisibleProperty) }
     }
 }
