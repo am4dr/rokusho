1b63f32185f5b07e9a77c5020d0ce43a002292e0
amadarain <t.f.octo@gmail.com>
2017-03-10 19:25:14 +0900

    Use ConcatenatedList in ImageLibraryCollection

--- src/main/kotlin/com/github/am4dr/rokusho/util/Utils.kt
+++ src/main/kotlin/com/github/am4dr/rokusho/util/Utils.kt
@@ -2,7 +2,20 @@ package com.github.am4dr.rokusho.util
 
 import javafx.beans.property.ListProperty
 import javafx.beans.property.SimpleListProperty
-import javafx.collections.FXCollections
+import javafx.collections.*
 
 fun <T> createEmptyListProperty(): ListProperty<T> =
         SimpleListProperty(FXCollections.observableList(mutableListOf<T>()))
+
+fun <T> toObservableList(observableSet: ObservableSet<T>): ObservableList<T> {
+    val list = object : ObservableList<T> by FXCollections.observableList(mutableListOf()), SetChangeListener<T> {
+        override fun onChanged(change: SetChangeListener.Change<out T>?) {
+            change?.run {
+                remove(elementRemoved)
+                add(elementAdded)
+            }
+        }
+    }
+    observableSet.addListener(WeakSetChangeListener(list))
+    return list
+}
