c05180f674546db5be4bef9b6be0ab6425af6a3b
amadarain <t.f.octo@gmail.com>
2017-03-10 17:55:01 +0900

    Add ConcatenatedList.concat()

--- src/test/kotlin/com/github/am4dr/rokusho/util/ConcatenatedListTest.kt
+++ src/test/kotlin/com/github/am4dr/rokusho/util/ConcatenatedListTest.kt
@@ -68,4 +68,41 @@ class ConcatenatedListTest {
             assertIterableEquals(listOf(100,0,300), concat)
         }
     }
+    @Nested
+    class MultipleListConcatenationTest {
+        private var left = observableList(mutableListOf(1, 2, 3))
+        private var middle = observableList(mutableListOf(0))
+        private var right = observableList(mutableListOf(100, 200, 300))
+        var concat = ConcatenatedList(left, middle, right)
+        @BeforeEach
+        fun beforeEach() {
+            left = observableList(mutableListOf(1, 2, 3))
+            middle = observableList(mutableListOf(0))
+            right = observableList(mutableListOf(100, 200, 300))
+            concat = ConcatenatedList(left, middle, right)
+        }
+        @Test
+        fun threeListsConcatenation() {
+            assertIterableEquals(listOf(1,2,3,0,100,200,300), concat)
+        }
+        @Test
+        fun modifyTheMiddleOfThreeListsConcatenation() {
+            middle.addAll(0, 0)
+            assertIterableEquals(listOf(1,2,3,0,0,0,100,200,300), concat)
+        }
+    }
+    @Nested
+    class ConcatTest {
+        @Test
+        fun concatMethod() {
+            val concat = ConcatenatedList<Int>()
+            val mid = observableList(mutableListOf(10,20,30))
+            concat.concat(observableList(listOf(1,2,3)))
+            concat.concat(mid)
+            concat.concat(observableList(listOf(100,200,300)))
+            assertIterableEquals(listOf(1,2,3,10,20,30,100,200,300), concat)
+            mid.clear()
+            assertIterableEquals(listOf(1,2,3,100,200,300), concat)
+        }
+    }
 }
