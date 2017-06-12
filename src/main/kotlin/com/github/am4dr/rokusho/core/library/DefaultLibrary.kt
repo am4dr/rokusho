7f7e8fb3eecf3179a625f961896ac0dfbf9095e6
amadarain <t.f.octo@gmail.com>
2017-06-12 16:09:01 +0900

    Rename cachedItems to watchedItems in DefaultLibrary

--- src/main/kotlin/com/github/am4dr/rokusho/core/library/DefaultLibrary.kt
+++ src/main/kotlin/com/github/am4dr/rokusho/core/library/DefaultLibrary.kt
@@ -13,28 +13,38 @@ class DefaultLibrary<T>(
         private val tags: MutableList<Tag> = mutableListOf(),
         private val itemTagDB: ItemTagDB<T> = SimpleItemTagDB()) : Library<T> {
 
-    private val itemCache = observableHashMap<T, Item<T>>()
-    private fun cacheItem(item: Item<T>) {
-        itemCache[item.key] = item
+    private val watchedItems = observableHashMap<T, Item<T>>()
+    private fun watchIfNotWatched(item: Item<T>) {
+        if (!watchedItems.containsKey(item.key)) { watchedItems[item.key] = item }
     }
 
     override fun getItemSet(list: Iterable<T>): ItemSet<T> {
-        val itemSet = DefaultLibraryItemSet(this, list.mapTo(observableArrayList(), this::getOrCreateEmptyItemOf))
-        itemCache.addListener(WeakMapChangeListener(itemSet))
+        val items = list.mapTo(observableArrayList(), this::getOrCreateEmptyItemOf)
+        items.forEach(this::watchIfNotWatched)
+        val itemSet = DefaultLibraryItemSet(this, items)
+        watchedItems.addListener(WeakMapChangeListener(itemSet))
         return itemSet
     }
+    private fun updateItem(item: Item<T>) {
+        itemTagDB.set(item.key, item.itemTags)
+        watchedItems[item.key]?.takeIf { it != item }
+                ?.let{ watchedItems[item.key] = item }
+    }
 
-    private  fun createItem(key: T, tags: Iterable<ItemTag>): Item<T>? = Item(key, tags.toList()).apply(this::cacheItem)
-    override fun getItem(key: T): Item<T>?                  = itemCache[key] ?: itemTagDB.get(key).takeIf { it.isNotEmpty() }?.let { createItem(key, it) }
-    override fun getOrCreateEmptyItemOf(key: T): Item<T>    = getItem(key) ?: createItem(key, listOf())!!
-    override fun addItem(key: T, tags: Iterable<ItemTag>)   = updateItemTags(key, tags)
+    override fun getItem(key: T): Item<T>?                  = watchedItems[key] ?: itemTagDB.get(key).takeIf { it.isNotEmpty() }?.let { Item(key, it) }
+    override fun getOrCreateEmptyItemOf(key: T): Item<T>    = getItem(key) ?: Item(key, listOf())
+    // TODO どう実装すべきか、あるいは削除するべきか
+    override fun addItem(key: T, tags: Iterable<ItemTag>) {
+        throw NotImplementedError()
+        updateItemTags(key, tags)
+    }
     override fun removeItem(key: T) {
+        throw NotImplementedError()
         itemTagDB.remove(key)
-        itemCache.remove(key)
+        watchedItems.remove(key)
     }
     override fun updateItemTags(key: T, tags: Iterable<ItemTag>) {
-        itemTagDB.set(key, tags.toList())
-        createItem(key, tags.toList())
+        updateItem(Item(key, tags.toList()))
     }
 }
 class DefaultLibraryItemSet<T>(override val library: Library<T>, target: ObservableList<Item<T>>) : ItemSet<T>, MapChangeListener<T, Item<T>> {
@@ -43,6 +53,7 @@ class DefaultLibraryItemSet<T>(override val library: Library<T>, target: Observa
     private val _items = observableArrayList(target)
     override val items: ReadOnlyListProperty<Item<T>> = ReadOnlyListWrapper(_items).readOnlyProperty
 
+    // TODO 更新(つまり削除と追加が同時に行われるもの)ではなくただの削除に対応する
     override fun onChanged(change: MapChangeListener.Change<out T, out Item<T>>?) {
         change ?: return
         val idx = values.indexOf(change.key).takeIf { it >= 0 } ?: return
