1612d0166ad4e98d1143a41d6c4e47861e760826
amadarain <t.f.octo@gmail.com>
2017-06-13 17:02:59 +0900

    Remove old doc comment

--- src/main/kotlin/com/github/am4dr/rokusho/core/library/Library.kt
+++ src/main/kotlin/com/github/am4dr/rokusho/core/library/Library.kt
@@ -2,11 +2,6 @@ package com.github.am4dr.rokusho.core.library
 
 import javafx.beans.property.ReadOnlyMapProperty
 
-/**
- * [Item]のコレクション
- *
- * [Item]は[Item.key]をキーとして識別可能。
- */
 interface Library<T> {
     fun getItem(key: T): Item<T>
     fun getItemSet(list: Iterable<T>): ItemSet<T>
