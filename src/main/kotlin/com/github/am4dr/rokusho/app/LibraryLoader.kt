57055a0dce5840cbebad64d6a59e92fdb127ca6c
amadarain <t.f.octo@gmail.com>
2017-06-14 15:00:13 +0900

    Change loadDirectory

--- src/main/kotlin/com/github/am4dr/rokusho/app/LibraryLoader.kt
+++ src/main/kotlin/com/github/am4dr/rokusho/app/LibraryLoader.kt
@@ -9,30 +9,42 @@ import java.nio.file.Path
 import java.util.stream.Collectors
 
 class LibraryLoader {
-    private data class LoadedLibrary constructor(val library: Library<ImageUrl>, val libraryDir: Path, val savefileDir: Path)
+    private data class LoadedLibrary (val library: Library<ImageUrl>, val libraryRoot: Path, val savefileDir: Path)
 
     private val savefileLoader = SaveFileLoader()
     private val loadedLibraries: MutableList<LoadedLibrary> = mutableListOf()
-    
+
+    // TODO test
     fun loadDirectory(directory: Path): Library<ImageUrl> {
-        return (findLibrary(directory)
-                ?: savefileLoader.locateSaveFilePath(directory)?.let { LoadedLibrary(savefileLoader.load(it), directory, it.parent).also { loadedLibraries.add(it) } }
-                ?: createLibrary(directory)).library
+        val loadedLibrary = findLibrary(directory)
+        val savefileLocation = savefileLoader.locateSaveFilePath(directory)?.normalize()
+
+        if (loadedLibrary == null && savefileLocation == null) return createLibrary(directory).library
+
+        val loadedLibraryNameCount = loadedLibrary?.libraryRoot?.nameCount ?: 0
+        val savefileLocationNameCount = savefileLocation?.nameCount ?: 0
+
+        return if (loadedLibraryNameCount >= savefileLocationNameCount) {
+            loadedLibrary!!
+        }
+        else {
+            LoadedLibrary(savefileLoader.load(savefileLocation!!), savefileLocation.parent, savefileLocation.parent).also { loadedLibraries.add(it) }
+        }.library
     }
+
     @Deprecated("一時的な実装")
     fun getItemSet(directory: Path, depth: Int): ItemSet<ImageUrl> {
         return (findLibrary(directory) ?:createLibrary(directory)).library.getItemSet(collectImageUrls(directory, depth))
     }
 
     private fun findLibrary(directory: Path): LoadedLibrary? =
-            loadedLibraries.map { it.libraryDir.normalize() to it }
-                    .filter { (libDir, _) -> directory.normalize().startsWith(libDir) }
-                    .maxBy { (libDir, _) -> libDir.nameCount }
-                    ?.second
+            loadedLibraries.filter { directory.normalize().startsWith(it.libraryRoot) }
+                    .maxBy { it.libraryRoot.nameCount }
 
     private fun createLibrary(directory: Path): LoadedLibrary =
-            LoadedLibrary(DefaultLibrary(), directory, directory).also { loadedLibraries.add(it) }
+            LoadedLibrary(DefaultLibrary(), directory.normalize(), directory.normalize()).also { loadedLibraries.add(it) }
 
+    // TODO 移動
     private fun collectImageUrls(directory: Path, depth: Int): List<ImageUrl> =
             Files.walk(directory, depth)
                     .filter(Rokusho.Companion::isSupportedImageFile)
