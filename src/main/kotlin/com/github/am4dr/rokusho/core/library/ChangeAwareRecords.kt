fced93671b3079d679d039ba5e6eac39ca2b0490
amadarain <t.f.octo@gmail.com>
2017-09-28 21:13:05 +0900

    Remove companion method and object expression from ChangeAwareRecords

--- src/main/kotlin/com/github/am4dr/rokusho/core/library/ChangeAwareRecords.kt
+++ src/main/kotlin/com/github/am4dr/rokusho/core/library/ChangeAwareRecords.kt
@@ -1,32 +1,34 @@
 package com.github.am4dr.rokusho.core.library
 
+import javafx.collections.FXCollections
 import javafx.collections.MapChangeListener
 import javafx.collections.ObservableList
 import javafx.collections.WeakMapChangeListener
 
-class ChangeAwareRecords<T>(records: ObservableList<Record<T>>, library: Library<T>): ObservableList<Record<T>> by create(records, library) {
-    companion object {
-        private fun <T> create(records: ObservableList<Record<T>>, library: Library<T>): ObservableList<Record<T>> {
-            return object : ObservableList<Record<T>> by records {
-                val tagsListener = MapChangeListener<String, Tag> { c ->
-                    records.forEachIndexed { index, record ->
-                        if (record.itemTags.any { it.tag.id == c.key }) {
-                            library.records[record.key]?.let { set(index, it) }
-                        }
-                    }
-                }
-                val recordListener = MapChangeListener<T, Record<T>> { c ->
-                    records.forEachIndexed { index, record ->
-                        if (record.key == c.key) {
-                            library.records[record.key]?.let { set(index, it) }
-                        }
-                    }
-                }
-                init {
-                    library.tags.addListener(WeakMapChangeListener(tagsListener))
-                    library.records.addListener(WeakMapChangeListener(recordListener))
-                }
+class ChangeAwareRecords<T>(records: List<Record<T>>, private val library: Library<T>) : ObservableList<Record<T>> by FXCollections.observableArrayList(records) {
+
+    private val tagsListener = MapChangeListener<String, Tag> { c ->
+        updateRecordsAll { it.itemTags.any { it.tag.id == c.key } }
+    }
+    private val recordListener = MapChangeListener<T, Record<T>> { c ->
+        updateRecordsAll { it.key == c.key }
+    }
+    private fun updateRecordsAll(start: Int = 0, predicate: (Record<T>)->Boolean) {
+        (start..lastIndex).forEach { index ->
+            val record = get(index).takeIf(predicate) ?: return@forEach
+            val original = library.records[record.key]
+            if (original === record) return@forEach
+
+            if (original == null) {
+                removeAt(index)
+                return updateRecordsAll(index, predicate)
             }
+            set(index, original)
         }
+    }
+
+    init {
+        library.tags.addListener(WeakMapChangeListener(tagsListener))
+        library.records.addListener(WeakMapChangeListener(recordListener))
     }
 }
