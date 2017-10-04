3e1210b8caecabe195dfff089dea99c1cd0213b1
amadarain <t.f.octo@gmail.com>
2017-10-04 16:59:04 +0900

    Add new LocalFileSystemLibraryLoader

---
+++ src/main/kotlin/com/github/am4dr/rokusho/app/library/lfs/SaveDataStoreProvider.kt
@@ -0,0 +1,31 @@
+package com.github.am4dr.rokusho.app.library.lfs
+
+import com.github.am4dr.rokusho.app.savedata.store.SaveDataStore
+import com.github.am4dr.rokusho.app.savefile.SaveData
+import java.nio.file.Path
+
+class SaveDataStoreProvider(private val saveDataStoreLocator: (Path) -> Pair<Path, SaveDataStore<SaveData>>,
+                            private val knownStore: MutableMap<Path, SaveDataStore<SaveData>> = mutableMapOf()) {
+
+    fun get(path: Path): Pair<Path, SaveDataStore<SaveData>> {
+        val known = findMostInnerKnownStore(path)
+        known?.let {
+            if  (it.first.toRealPath() == path.toRealPath()) {
+                return it
+            }
+        }
+        val located = saveDataStoreLocator(path)
+
+        val mostInner = if (known != null && known.first.toRealPath().nameCount >= located.first.toRealPath().nameCount) {
+            known
+        }
+        else {
+            located
+        }
+        return mostInner.apply {
+            knownStore[first.toRealPath()] = second
+        }
+    }
+    private fun findMostInnerKnownStore(path: Path): Pair<Path, SaveDataStore<SaveData>>? =
+            knownStore.filterKeys { path.startsWith(it) }.maxBy { it.key.nameCount }?.toPair()
+}
