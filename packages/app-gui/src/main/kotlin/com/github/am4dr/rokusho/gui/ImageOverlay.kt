dd0398c6dee7b83b9b3829f9525d4950503b8e92
amadarain <t.f.octo@gmail.com>
2018-04-20 18:33:20 +0900

    Add ImageOverlay.Companion#attach(Node)

--- packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/ImageOverlay.kt
+++ packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/ImageOverlay.kt
@@ -5,11 +5,22 @@ import javafx.beans.property.ObjectProperty
 import javafx.beans.property.SimpleDoubleProperty
 import javafx.beans.property.SimpleObjectProperty
 import javafx.geometry.Pos
+import javafx.scene.Node
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
+import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
 
 class ImageOverlay : VBox() {
+
+    companion object {
+        fun <T : Node> attach(node: T): Triple<StackPane, T, ImageOverlay> {
+            val overlay = ImageOverlay()
+            val stackPane = StackPane(node, overlay)
+            return Triple(stackPane, node, overlay)
+        }
+    }
+
     val widthRatioProperty: DoubleProperty = SimpleDoubleProperty(0.8)
     val heightRatioProperty: DoubleProperty = SimpleDoubleProperty(0.8)
     private val imageView = ImageView()
