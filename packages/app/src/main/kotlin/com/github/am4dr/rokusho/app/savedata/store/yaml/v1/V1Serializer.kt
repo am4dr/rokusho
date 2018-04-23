ca1cb48b43c1764bb1fe9a0cc64a15b21e3a1831
amadarain <t.f.octo@gmail.com>
2018-04-23 23:59:10 +0900

    Remove empty data items from serialized SaveData

--- packages/app/src/main/kotlin/com/github/am4dr/rokusho/app/savedata/store/yaml/v1/V1Serializer.kt
+++ packages/app/src/main/kotlin/com/github/am4dr/rokusho/app/savedata/store/yaml/v1/V1Serializer.kt
@@ -10,11 +10,13 @@ fun V1SaveData.toDumpStructure(): Map<Any, Any> {
     return mapOf(
             "version" to SaveData.Version.VERSION_1.stringValue,
             "tags" to tags.map { it.toDumpStructure() }.toMap().toMutableMap(),
-            "metaData" to items.map { it.toDumpStructure() }.toMap().toMutableMap()
+            "metaData" to items.map { it.toDumpStructure() }.filter { (_, data) -> data.isNotEmpty() }.toMap().toMutableMap()
             )
 }
 
 fun TagEntry.toDumpStructure(): Pair<String, Any> = id to data
-fun ItemEntry.toDumpStructure(): Pair<String, Any> = id to mapOf("tags" to tags.map { it.toDumpStructure() }.toMap().toMutableMap())
+fun ItemEntry.toDumpStructure(): Pair<String, Map<String, Any>> {
+    val tags = tags.map { it.toDumpStructure() }.toMap().toMutableMap()
+    return id to if (tags.isNotEmpty()) mapOf("tags" to tags) else mutableMapOf()
+}
 fun ItemTagEntry.toDumpStructure(): Pair<String, Any> = id to data
-
