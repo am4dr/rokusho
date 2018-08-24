b04da37270628fedd51614596004136ae51661a9
amadarain <t.f.octo@gmail.com>
2018-08-24 23:11:15 +0900

    add CharacterIcon for SimpleSideMenu

--- packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/GUIModel.kt
+++ packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/GUIModel.kt
@@ -1,6 +1,7 @@
 package com.github.am4dr.rokusho.gui
 
 import com.github.am4dr.rokusho.app.library.RokushoLibrary
+import com.github.am4dr.rokusho.gui.old.sidemenu.CharacterIcon
 import com.github.am4dr.rokusho.gui.old.sidemenu.SideMenuIcon
 import com.github.am4dr.rokusho.javafx.collection.TransformedList
 import javafx.beans.binding.Bindings
@@ -9,15 +10,11 @@ import javafx.beans.value.ObservableValue
 import javafx.collections.ObservableList
 import javafx.geometry.Insets
 import javafx.scene.Node
-import javafx.scene.control.Label
 import javafx.scene.control.Tooltip
-import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.Background
 import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.CornerRadii
 import javafx.scene.paint.Color
-import javafx.scene.text.Font
-import java.util.concurrent.Callable
 
 class GUIModel(val libraryCollection: LibraryCollection,
                private val libraryViewerRepository: LibraryViewerRepository) {
@@ -35,22 +32,12 @@ class GUIModel(val libraryCollection: LibraryCollection,
         libraryCollection.selectedProperty().get()?.let(libraryViewerRepository::get)
     }, arrayOf(libraryCollection.selectedProperty()))
 
-    // TODO separate data and styling
     private fun RokushoLibrary<*>.toSideMenuIcon(): SideMenuIcon =
-            SideMenuIcon().apply {
+            CharacterIcon().apply {
                 Tooltip.install(this, Tooltip(name))
                 backgroundProperty().bind(When(selectedProperty)
                         .then(Background(BackgroundFill(Color.INDIANRED, CornerRadii(4.0), Insets.EMPTY)))
                         .otherwise(Background(BackgroundFill(Color.ANTIQUEWHITE, CornerRadii(4.0), Insets.EMPTY))))
-                val firstLetter = Label(this@toSideMenuIcon.shortName.first().toString()).apply {
-                    fontProperty().bind(Bindings.createObjectBinding(Callable { Font(size.get() * 0.7) }, size))
-                }
-                AnchorPane.setTopAnchor(firstLetter, 0.0)
-                AnchorPane.setLeftAnchor(firstLetter, 6.0)
-                val libLabel = Label("Lib:")
-                AnchorPane.setTopAnchor(libLabel, 0.0)
-                AnchorPane.setLeftAnchor(libLabel, 0.0)
-                val labels = AnchorPane(firstLetter, libLabel)
-                children.add(labels)
+                character.set(this@toSideMenuIcon.shortName.first().toString())
             }
 }
---
+++ packages/gui/src/main/kotlin/com/github/am4dr/rokusho/gui/old/sidemenu/CharacterIcon.kt
@@ -0,0 +1,33 @@
+package com.github.am4dr.rokusho.gui.old.sidemenu
+
+import javafx.beans.InvalidationListener
+import javafx.beans.binding.Bindings
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
+import javafx.scene.control.Label
+import javafx.scene.layout.AnchorPane
+import javafx.scene.text.Font
+import java.util.concurrent.Callable
+
+class CharacterIcon : SideMenuIcon() {
+
+    val character: StringProperty = SimpleStringProperty("*")
+    private val fontSize = size.multiply(0.7)
+    private val characterLeftMargin = size.subtract(fontSize).divide(2)
+
+    private val characterLabel = Label().apply {
+        textProperty().bind(character)
+        fontProperty().bind(Bindings.createObjectBinding(Callable { Font(fontSize.get()) }, fontSize, character))
+    }
+
+    init {
+        updateLetterAnchor()
+        characterLeftMargin.addListener(InvalidationListener { updateLetterAnchor() })
+        children.add(AnchorPane(characterLabel))
+    }
+
+    private fun updateLetterAnchor() {
+        AnchorPane.setTopAnchor(characterLabel, 0.0)
+        AnchorPane.setLeftAnchor(characterLabel, characterLeftMargin.get())
+    }
+}
