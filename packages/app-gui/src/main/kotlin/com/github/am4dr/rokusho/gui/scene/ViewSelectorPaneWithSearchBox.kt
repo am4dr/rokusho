567e6039ae5a3342202e3b0ab3e04273bc872092
amadarain <t.f.octo@gmail.com>
2018-09-02 07:42:19 +0900

    replace RecordsViewerContainer with ViewSelectorPaneWithSearchBox

---
+++ packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/scene/ViewSelectorPaneWithSearchBox.kt
@@ -0,0 +1,65 @@
+package com.github.am4dr.rokusho.gui.scene
+
+import com.github.am4dr.rokusho.javafx.collection.TransformedList
+import javafx.beans.binding.Bindings
+import javafx.beans.binding.Bindings.bindContent
+import javafx.beans.binding.Bindings.createObjectBinding
+import javafx.beans.property.LongProperty
+import javafx.beans.property.ReadOnlyObjectWrapper
+import javafx.beans.property.ReadOnlyStringProperty
+import javafx.beans.property.SimpleLongProperty
+import javafx.collections.FXCollections
+import javafx.collections.ListChangeListener
+import javafx.collections.ObservableList
+import javafx.geometry.Pos
+import javafx.scene.Node
+import javafx.scene.control.Button
+import javafx.scene.control.ContentDisplay
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.layout.*
+import java.util.concurrent.Callable
+
+class ViewSelectorPaneWithSearchBox : VBox() {
+
+    val selections: ObservableList<Selection> = FXCollections.observableArrayList()
+
+    fun filterPassedCountProperty(): LongProperty = SimpleLongProperty(0)
+    fun totalCountProperty(): LongProperty = SimpleLongProperty(0)
+    fun filterTextProperty(): ReadOnlyStringProperty = filterInputNode.textProperty()
+
+
+    private val filterInputNode = TextField()
+    private val buttons = TransformedList(selections) { Button(it.label).apply { setOnAction { _ -> it.select() } } }
+
+    private val selected = ReadOnlyObjectWrapper<Selection>()
+
+    init {
+        val buttonsPane = HBox().apply { bindContent(children, buttons) }
+        val filterPane = HBox(
+                Label("フィルター", filterInputNode).apply { contentDisplay = ContentDisplay.RIGHT },
+                Label().apply { textProperty().bind(Bindings.concat("[", filterPassedCountProperty(),  " / ", totalCountProperty(), "]")) }
+        ).apply { alignment = Pos.BASELINE_LEFT }
+        val topPanePadding = Pane().apply {
+            prefWidth = 0.0
+            HBox.setHgrow(this, Priority.ALWAYS)
+        }
+        val contentPane = BorderPane().apply {
+            VBox.setVgrow(this, Priority.ALWAYS)
+            centerProperty().bind(createObjectBinding(Callable { selected.get()?.node }, selected))
+        }
+        children.addAll(
+                HBox(buttonsPane, topPanePadding, filterPane),
+                contentPane
+        )
+
+        selections.addListener(ListChangeListener { if (it.list.size == 1) { select(0) } })
+    }
+
+    private fun select(n: Int) = selections.getOrNull(n)?.select()
+
+    private fun Selection.select() = selected.set(this)
+
+
+    class Selection(val label: String, val node: Node)
+}
--- packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/viewer/factory/RecordsViewersLibraryViewerFactory.kt
+++ packages/app-gui/src/main/kotlin/com/github/am4dr/rokusho/gui/viewer/factory/RecordsViewersLibraryViewerFactory.kt
@@ -2,12 +2,15 @@ package com.github.am4dr.rokusho.gui.viewer.factory
 
 import com.github.am4dr.rokusho.app.library.RokushoLibrary
 import com.github.am4dr.rokusho.core.library.Record
+import com.github.am4dr.rokusho.gui.scene.ViewSelectorPaneWithSearchBox
 import com.github.am4dr.rokusho.gui.viewer.LibraryViewerFactory
 import com.github.am4dr.rokusho.javafx.function.bindLeft
 import javafx.beans.binding.Bindings
+import javafx.beans.binding.Bindings.bindContent
 import javafx.beans.property.SimpleListProperty
 import javafx.collections.transformation.FilteredList
 import javafx.scene.Node
+import javafx.scene.layout.StackPane
 import java.util.function.Predicate
 import kotlin.reflect.KClass
 
@@ -15,24 +18,33 @@ class RecordsViewersLibraryViewerFactory(private val recordsViewerFactories: Lis
 
     override fun create(library: RokushoLibrary<*>): Node = createLibraryViewer(library.type, library)
 
-    private fun <T : Any> createLibraryViewer(type: KClass<T>, library: RokushoLibrary<*>): Node = RecordsViewerContainer<T>().apply {
+    private fun <T : Any> createLibraryViewer(type: KClass<T>, library: RokushoLibrary<*>): Node {
         assert(type == library.type)
+
         @Suppress("UNCHECKED_CAST")
         library as RokushoLibrary<T>
 
-        records.bind(SimpleListProperty(FilteredList(library.records).apply {
-            val recordFilter = byTagNameRecordFilterFactory.bindLeft(filterProperty)
-            predicateProperty().bind(recordFilter)
-        }))
-        totalCount.bind(Bindings.size(library.records))
-        filteredCount.bind(Bindings.size(records))
-
-        recordsViewerFactories
-                .filter { it.isAcceptable(library.type) }
-                .mapNotNull { it.create(library) }
-                .forEach { add(it) }
+        return object : StackPane() {
+            val filteredList = FilteredList(library.records)
+            val filteredRecords = SimpleListProperty(filteredList)
+            init {
+                val view = ViewSelectorPaneWithSearchBox().apply {
+                    totalCountProperty().bind(Bindings.size(library.records))
+                    filterPassedCountProperty().bind(Bindings.size(filteredRecords))
+                }
+                val recordFilter = byTagNameRecordFilterFactory.bindLeft(view.filterTextProperty())
+                filteredList.predicateProperty().bind(recordFilter)
+                recordsViewerFactories
+                        .filter { it.isAcceptable(library.type) }
+                        .mapNotNull { it.create(library) }
+                        .forEach {
+                            bindContent(it.records, filteredRecords)
+                            view.selections.add(ViewSelectorPaneWithSearchBox.Selection(it.label, it.viewer))
+                        }
+                children.add(view)
+            }
+        }
     }
-
     private val byTagNameRecordFilterFactory = { input: String? ->
         Predicate { item: Record<*> ->
             if (input == null || input == "") true
